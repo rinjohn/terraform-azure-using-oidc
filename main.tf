3a99e2f4ecdd5077865bbdff65694bda25031de5
rinjohn <43170752+rinjohn@users.noreply.github.com>
2024-10-27 11:21:07 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "azurerm" {
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+variable "resource_group_location" {
+  type        = string
+  default     = "uksouth"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rgs"
+  description = "Prefix of the resource group name combined with a random ID."
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
